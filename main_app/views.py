6eb4f6e2d7588e479be24fd4dce99642eab4c97e
Ryan Combest <crcombest@gmail.com>
2022-11-05 19:50:42 -0500

    renders about page with base html and adds styling

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>Henlo</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the RecordCollector</h1>')
+  return render(request, 'about.html')
