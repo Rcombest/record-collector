19f8ed4a828683fc39900b04203879bbf3686973
Ryan Combest <crcombest@gmail.com>
2022-11-06 14:18:29 -0600

    renders records index using record database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+  name = models.CharField(max_length=100)
+  artist = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
+  release_year = models.IntegerField()
+  
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Record:
-  def __init__(self, name, artist, description, release_year):
-    self.name = name
-    self.artist = artist
-    self.description = description
-    self.release_year = release_year
-
-records = [
-  Record("God's Country", "Chat Pile", "Bone-crushing, homegrown American nihilism", 2022),
-  Record("The Expanding Universe", "Laurie Spiegel", "Beautiful synth soundscapes to soothe the soul", 1980),
-  Record("The Epic", "Kamasi Washington", "Fantastic three hour modern jazz odyssey", 2015)
-]
+from .models import Record
 
 # Create your views here.
 def home(request):
@@ -22,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def records_index(request):
+  records = Record.objects.all()
   return render(request, 'records/index.html', { 'records': records })
