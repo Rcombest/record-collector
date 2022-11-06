5b20a558c90616c83e86c9404c0c9411d862acad
Ryan Combest <crcombest@gmail.com>
2022-11-05 21:28:38 -0500

    renders records index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('records/', views.records_index, name='records_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Record:
+  def __init__(self, name, artist, description, release_year):
+    self.name = name
+    self.artist = artist
+    self.description = description
+    self.release_year = release_year
+
+records = [
+  Record("God's Country", "Chat Pile", "Bone-crushing, homegrown American nihilism", 2022),
+  Record("The Expanding Universe", "Laurie Spiegel", "Beautiful synth soundscapes to soothe the soul", 1980),
+  Record("The Epic", "Kamasi Washington", "Fantastic three hour modern jazz odyssey", 2015)
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Henlo</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def records_index(request):
+  return render(request, 'records/index.html', { 'records': records })
