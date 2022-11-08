813407d5a3ccd44286843de7958988f3d61093c5
Ryan Combest <crcombest@gmail.com>
2022-11-07 20:44:43 -0600

    implements creation of a single cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Record(models.Model):
@@ -9,3 +10,7 @@ class Record(models.Model):
   
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+      return reverse("records_detail", kwargs={"record_id": self.id})
+  
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('records/', views.records_index, name='records_index'),
   path('records/<int:record_id>/', views.records_detail, name='records_detail'),
+  path('records/create/', views.RecordCreate.as_view(), name='records_create'),
 ]
