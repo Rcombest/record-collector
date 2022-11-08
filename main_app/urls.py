12165fb46c2ea3d2135422e94380014c97129780
Ryan Combest <crcombest@gmail.com>
2022-11-07 21:18:51 -0600

    implements update and delete functionality for records

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('records/', views.records_index, name='records_index'),
   path('records/<int:record_id>/', views.records_detail, name='records_detail'),
   path('records/create/', views.RecordCreate.as_view(), name='records_create'),
+  path('records/<int:pk>/update/', views.RecordUpdate.as_view(), name='records_update'),
+  path('records/<int:pk>/delete/', views.RecordDelete.as_view(), name='records_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Record
 
 # Create your views here.
@@ -20,3 +20,11 @@ def records_detail(request, record_id):
 class RecordCreate(CreateView):
   model = Record
   fields = '__all__'
+
+class RecordUpdate(UpdateView):
+  model = Record
+  fields = ['name', 'artist', 'description', 'release_year']
+
+class RecordDelete(DeleteView):
+  model = Record
+  success_url = '/records/'
