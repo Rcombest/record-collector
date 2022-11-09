e60016a5ba7b33344c4f2c3aca0458a237de2078
Ryan Combest <crcombest@gmail.com>
2022-11-09 01:13:55 -0600

    implements auth on view functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from .models import Record, Song
@@ -12,10 +15,12 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def records_index(request):
-  records = Record.objects.all()
+  records = Record.objects.filter(user=request.user)
   return render(request, 'records/index.html', { 'records': records })
 
+@login_required
 def records_detail(request, record_id):
   record = Record.objects.get(id=record_id)
   songs_record_doesnt_have = Song.objects.exclude(id__in = record.songs.all().values_list('id'))
@@ -42,6 +47,7 @@ class RecordDelete(DeleteView):
   model = Record
   success_url = '/records/'
 
+@login_required
 def add_spun(request, record_id):
   form = SpunForm(request.POST)
   if form.is_valid():
@@ -68,6 +74,21 @@ class SongDelete(DeleteView):
   model = Song
   success_url = '/songs/'
 
+@login_required
 def assoc_song(request, record_id, song_id):
   Record.objects.get(id=record_id).songs.add(song_id)
   return redirect('records_detail', record_id=record_id)
+
+def signup(request):
+  error_message = ""
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('records_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
