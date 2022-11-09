649c3d20ce9bd7ba2a6423e6646e16dd63a8ecec
Ryan Combest <crcombest@gmail.com>
2022-11-08 23:57:10 -0600

    begins implementation of auth and renders login page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('records/', views.records_index, name='records_index'),
   path('records/<int:record_id>/', views.records_detail, name='records_detail'),
