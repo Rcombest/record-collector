3880b358dd94a8e7603ba477062954f1a8eed9f2
Ryan Combest <crcombest@gmail.com>
2022-11-14 09:16:58 -0600

    config deployment

--- recordcollector/settings.py
+++ recordcollector/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 from pathlib import Path
 import os
 
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6*fmln)n140krac(l(r9o6qxkjz^j()184f&z+-u!u_wc!0%#j'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +137,10 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# configure django app for heroku
+import django_heroku
+
+django_heroku.settings(locals())
+
+DEBUG_PROPAGATE_EXCEPTIONS = True 
